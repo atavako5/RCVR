fd0a33b3daf0d98e52f1c1775943091817e0cfcc
Ali Tavakoli <atavako5@uwo.ca>
2019-09-22 17:51:44 -0400

    RCVR

---
+++ Assets/WebCamCopy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamCopy : MonoBehaviour {
+    public RawImage rawimageCopiedFrom;
+    public RawImage rawimageCopiedTo;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rawimageCopiedTo.texture = rawimageCopiedFrom.texture;
+        rawimageCopiedTo.material.mainTexture = rawimageCopiedFrom.material.mainTexture;
+    }
+}
---
+++ Assets/webcam2S.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webcam2S : MonoBehaviour {
+    public int webcamNum;
+    public RawImage rawimage;
+
+    // Use this for initialization
+    void Start () {
+
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Debug.Log(WebCamTexture.devices[webcamNum].name);
+        webcamTexture.deviceName = WebCamTexture.devices[webcamNum].name;
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+	
+}
